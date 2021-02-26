84b0ea0ecfe576d0ed433318c1c6e1ee1d759bdf
Idris Adebayo <72842224+IdrisAdebayo@users.noreply.github.com>
2021-02-26 01:51:34 -0600

    changed directories

---
+++ script.js
@@ -0,0 +1,18 @@
+//generate random password
+function generate(){
+
+  //set password length/complexity
+  let complexity = document.getElementById("slider").value;
+
+  //possible password values
+  const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+  let password = "";
+
+  //create for loop to choose password characters
+
+  for (var i = 0; i <= complexity; i++){
+    password = password
+  }
+
+}
