0b4d16203dc790605074c434b637acb1f837c85f
Idris Adebayo <72842224+IdrisAdebayo@users.noreply.github.com>
2021-03-01 15:11:27 -0600

    added  new codes

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ function generate(){
   let complexity = document.getElementById("slider").value;
 
   //possible password values
-  const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+  const values = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
   let password = "";
 
   //create for loop to choose password characters
 
-  for (var i = 0; i <= complexity; i++){
-    password = password
+  for(var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
 
+  //Add password to textbox/display area
+  document.getElementById("display").value = password
 }
