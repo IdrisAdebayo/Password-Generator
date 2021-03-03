434532ffd581dca51ecce5fc069f4176fe9165b1
Idris Adebayo <72842224+IdrisAdebayo@users.noreply.github.com>
2021-03-03 14:36:25 -0600

    added a key functional line to js code, added a ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Password-Generator
 A website that generates random passwords
+
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Usage](#usage)
+* [deployed link](#deployed link)
+* [description](#description)
+* [Credits](#credits)
+
+
+## Usage 
+Image of finished project
+![image of deployed app](./assets/images/portfolio.jpg)
+
+## deployed link: https://idrisadebayo.github.io/Password-Generator/
+
+## description
+In building up my profile as a developer, I attempted this project to practice the javascript skills I got introduced to in the past week.
+
+The user uses a slider to set random password length to determine length that is preset between  8 and 128 characters picked randomly from lowercase letters, uppercase letters, numbers and special characters.
+
+A key aspect of Javascript that stuck with me here is the iteration and the practice of creating    and calling functions.
+
+Seeing how to use a combination of the Math.random and the Math.floor function was deeply satisfying
+
+## Credits
+My most profound gratitude goes to Andrew Kemp for helping look through a problematic aspect of the code.
+
+I need to also gice credit to my class Instructor Jon Lynch and Cody
+
+I believe it is the further step to a wider world of possibilities in Software development
+
+
--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-//generate random password
+// Array of special characters to be included in password
+var specialCharacters = ['~','`','!','@','#','$','%','^','&','*','(',')','-','_','+','<','>','?'];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['1','2','3','4','5','6','7','8','9','0'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+
+
 function generate(){
 
   //set password length/complexity
   let complexity = document.getElementById("slider").value;
 
   //possible password values
-  const values = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
   let password = "";
 
   //create for loop to choose password characters
 
-  for(var i = 0; i <= complexity; i++){
+  for (var i = 0; i <= complexity; i++){
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
 
   //Add password to textbox/display area
   document.getElementById("display").value = password
 }
+
+generate();
+
+
